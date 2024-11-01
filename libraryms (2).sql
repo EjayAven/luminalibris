94ff4f7a68426f9ee0d981a5583e6e1a6c700823
EjayAven <amphixusejay@gmail.com>
2024-11-01 13:26:03 +0800

    Add files via upload

---
+++ libraryms (2).sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2024 at 05:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `libraryms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+-- Error reading structure for table libraryms.accounts: #1932 - Table &#039;libraryms.accounts&#039; doesn&#039;t exist in engine
+-- Error reading data for table libraryms.accounts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `libraryms`.`accounts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fines`
+--
+-- Error reading structure for table libraryms.fines: #1932 - Table &#039;libraryms.fines&#039; doesn&#039;t exist in engine
+-- Error reading data for table libraryms.fines: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `libraryms`.`fines`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materials`
+--
+-- Error reading structure for table libraryms.materials: #1932 - Table &#039;libraryms.materials&#039; doesn&#039;t exist in engine
+-- Error reading data for table libraryms.materials: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `libraryms`.`materials`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+-- Error reading structure for table libraryms.reservation: #1932 - Table &#039;libraryms.reservation&#039; doesn&#039;t exist in engine
+-- Error reading data for table libraryms.reservation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `libraryms`.`reservation`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
